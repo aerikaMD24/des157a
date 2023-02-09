653ffb84a20ac1b91be18e69065899af30ea3cb7
aerikaMD24 <aerika_md24@yahoo.com>
2023-02-08 17:10:14 -0800

    fix htm/csss validation

--- studio1/script.js
+++ studio1/script.js
@@ -3,4 +3,11 @@ g(function(){
     'use strict';
     console.log('reading js')
 
+    const myForm = document.querySelector('form');
+
+    document.addEventListener('submit', function(e){
+        e.preventDefault();
+
+    })
+
 }())
